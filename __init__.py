a567eda34cb9583af039fa8b06aae4820a72f29b
bagussr <bagusssyamsu@gmail.com>
2022-08-17 23:03:00 +0700

    add cors middleware

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src import *
@@ -7,5 +8,7 @@ from src.endpoint.main import route
 
 app = FastAPI(title="My Money Manager", description="Manage money our money flow from website", version="1.0.0")
 
-
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
 app.include_router(route)
